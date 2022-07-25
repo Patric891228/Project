e0b00700a0ea727af0b51dbb27aeed2ccc7b0a6e
Patric891228 <cchs.stu21047@gmail.com>
2022-07-25 13:32:18 +0800

    Create Information

---
+++ app/src/main/java/tw/scu/edu/graduationprojrct/MainScene.java
@@ -0,0 +1,14 @@
+package tw.scu.edu.graduationprojrct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_scene);
+    }
+}
