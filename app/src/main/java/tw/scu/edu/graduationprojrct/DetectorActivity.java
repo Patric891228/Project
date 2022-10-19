9cf20796680e062bca135c274f07124bb5240fe7
wang <wang890921@gmail.com>
2022-10-19 09:47:52 +0800

    修整程式碼

--- app/src/main/java/tw/scu/edu/graduationprojrct/DetectorActivity.java
+++ app/src/main/java/tw/scu/edu/graduationprojrct/DetectorActivity.java
@@ -35,27 +35,11 @@ import android.util.Log;
 import android.util.Size;
 import android.util.TypedValue;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.camera.core.CameraInfoUnavailableException;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.google.mlkit.common.MlKitException;
-import com.google.mlkit.vision.pose.PoseDetectorOptionsBase;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -69,9 +53,7 @@ import tw.scu.edu.graduationprojrct.customview.OverlayView.DrawCallback;
 import tw.scu.edu.graduationprojrct.env.BorderedText;
 import tw.scu.edu.graduationprojrct.env.ImageUtils;
 import tw.scu.edu.graduationprojrct.env.Logger;
-import tw.scu.edu.graduationprojrct.java.posedetector.PoseDetectorProcessor;
 import tw.scu.edu.graduationprojrct.java.posedetector.PoseGraphic;
-import tw.scu.edu.graduationprojrct.preference.PreferenceUtils;
 import tw.scu.edu.graduationprojrct.preference.SettingsActivity;
 import tw.scu.edu.graduationprojrct.scene.SportResultScene;
 import tw.scu.edu.graduationprojrct.tflite.Classifier;
@@ -84,7 +66,7 @@ import tw.scu.edu.graduationprojrct.tracking.MultiBoxTracker;
  * objects.
  */
 public class DetectorActivity extends CameraActivity
-        implements OnImageAvailableListener, AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
+        implements OnImageAvailableListener {
     private static final Logger LOGGER = new Logger();
 
     private static final DetectorMode MODE = DetectorMode.TF_OD_API;
@@ -114,28 +96,6 @@ public class DetectorActivity extends CameraActivity
 
     private BorderedText borderedText;
 
-    private static final String TAG = "CameraXLivePreview";
-    private static final String POSE_DETECTION = "Pose Detection";
-
-    private static final String STATE_SELECTED_MODEL = "selected_model";
-
-    private PreviewView previewView;
-    private GraphicOverlay graphicOverlay;
-
-    @Nullable
-    private ProcessCameraProvider cameraProvider;
-    @Nullable
-    private Preview previewUseCase;
-    @Nullable
-    private ImageAnalysis analysisUseCase;
-    @Nullable
-    private VisionImageProcessor imageProcessor;
-    private boolean needUpdateGraphicOverlayImageSourceInfo;
-
-    private String selectedModel = POSE_DETECTION;
-    private int lensFacing = CameraSelector.LENS_FACING_BACK;
-    private CameraSelector cameraSelector;
-
     String SportType_String;
     String Current_Sport;
     TextView Time_Word;
@@ -150,7 +110,7 @@ public class DetectorActivity extends CameraActivity
     int Order = -1;
     int ImgOrder;
     SportType ST;
-    int Success,Fail=0;
+    int Success, Fail = 0;
 
     public PoseGraphic PG;
     public String CurrentPose = "";
@@ -159,37 +119,6 @@ public class DetectorActivity extends CameraActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate");
-
-
-        if (savedInstanceState != null) {
-            selectedModel = savedInstanceState.getString(STATE_SELECTED_MODEL, POSE_DETECTION);
-        }
-        cameraSelector = new CameraSelector.Builder().requireLensFacing(lensFacing).build();
-
-//        setContentView(R.layout.activity_vision_camerax_live_preview);
-        previewView = findViewById(R.id.preview_view);
-        if (previewView == null) {
-            Log.d(TAG, "previewView is null");
-        }
-        graphicOverlay = findViewById(R.id.graphic_overlay);
-        if (graphicOverlay == null) {
-            Log.d(TAG, "graphicOverlay is null");
-        }
-
-        ToggleButton facingSwitch = findViewById(R.id.facing_switch);
-        facingSwitch.setOnCheckedChangeListener(this);
-
-        new ViewModelProvider(this, (ViewModelProvider.Factory) ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()))
-                .get(CameraXViewModel.class)
-                .getProcessCameraProvider()
-                .observe(
-                        this,
-                        provider -> {
-                            cameraProvider = provider;
-                            bindAllCameraUseCases();
-                        });
-
         shared = getSharedPreferences("data", MODE_PRIVATE);
         SportType_String = shared.getString("SportType", "Belly");
 //        綁定物件
@@ -211,8 +140,8 @@ public class DetectorActivity extends CameraActivity
         ST = new SportType(SportType_String);
         Log.d("ChooseSport", SportType_String);
         ST.LoadSportData();
-        for(int i =0;i<ST.SportContent.length;i++){
-            Log.d("該做動作",ST.SportContent[i]);
+        for (int i = 0; i < ST.SportContent.length; i++) {
+            Log.d("該做動作", ST.SportContent[i]);
         }
 
 //        初始化
@@ -227,11 +156,11 @@ public class DetectorActivity extends CameraActivity
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        CurrentTime-=10;//時間倒數
+                        CurrentTime -= 10;//時間倒數
                         Time_Word.setText(CurrentTime + "");
                         //if判斷示裡面放置在時間結束後想要完成的事件
                         if (CurrentTime < 1) {
-                            if (Order < ST.SportContentTime.length-1) {
+                            if (Order < ST.SportContentTime.length - 1) {
                                 if (isSport) {//進入休息狀態
                                     isSport = false;
                                     Next_Sport_Word.setImageDrawable(null);
@@ -249,7 +178,6 @@ public class DetectorActivity extends CameraActivity
                                     Order++; //讓時間執行緒保持輪迴
                                     ImgOrder++;
                                     CurrentTime = ST.SportContentTime[Order];
-
                                 }
                             } else {
                                 timer.cancel();
@@ -280,190 +208,6 @@ public class DetectorActivity extends CameraActivity
                             SettingsActivity.LaunchSource.CAMERAX_LIVE_PREVIEW);
                     startActivity(intent);
                 });
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle bundle) {
-        super.onSaveInstanceState(bundle);
-        bundle.putString(STATE_SELECTED_MODEL, selectedModel);
-    }
-
-    @Override
-    public synchronized void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-        selectedModel = parent.getItemAtPosition(pos).toString();
-        Log.d(TAG, "Selected model: " + selectedModel);
-        bindAnalysisUseCase();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Do nothing.
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (cameraProvider == null) {
-            return;
-        }
-        int newLensFacing =
-                lensFacing == CameraSelector.LENS_FACING_FRONT
-                        ? CameraSelector.LENS_FACING_BACK
-                        : CameraSelector.LENS_FACING_FRONT;
-        CameraSelector newCameraSelector =
-                new CameraSelector.Builder().requireLensFacing(newLensFacing).build();
-        try {
-            if (cameraProvider.hasCamera(newCameraSelector)) {
-                Log.d(TAG, "Set facing to " + newLensFacing);
-                lensFacing = newLensFacing;
-                cameraSelector = newCameraSelector;
-                bindAllCameraUseCases();
-                return;
-            }
-        } catch (CameraInfoUnavailableException e) {
-            // Falls through
-        }
-        Toast.makeText(
-                getApplicationContext(),
-                "This device does not have lens with facing: " + newLensFacing,
-                Toast.LENGTH_SHORT)
-                .show();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        bindAllCameraUseCases();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (imageProcessor != null) {
-            imageProcessor.stop();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (imageProcessor != null) {
-            imageProcessor.stop();
-        }
-    }
-
-    private void bindAllCameraUseCases() {
-        if (cameraProvider != null) {
-            // As required by CameraX API, unbinds all use cases before trying to re-bind any of them.
-            cameraProvider.unbindAll();
-            bindPreviewUseCase();
-            bindAnalysisUseCase();
-        }
-    }
-
-    private void bindPreviewUseCase() {
-        if (!PreferenceUtils.isCameraLiveViewportEnabled(this)) {
-            return;
-        }
-        if (cameraProvider == null) {
-            return;
-        }
-        if (previewUseCase != null) {
-            cameraProvider.unbind(previewUseCase);
-        }
-
-        Preview.Builder builder = new Preview.Builder();
-        Size targetResolution = PreferenceUtils.getCameraXTargetResolution(this, lensFacing);
-        if (targetResolution != null) {
-            builder.setTargetResolution(targetResolution);
-        }
-        previewUseCase = builder.build();
-        previewUseCase.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle(/* lifecycleOwner= */ this, cameraSelector, previewUseCase);
-    }
-
-    private void bindAnalysisUseCase() {
-        if (cameraProvider == null) {
-            return;
-        }
-        if (analysisUseCase != null) {
-            cameraProvider.unbind(analysisUseCase);
-        }
-        if (imageProcessor != null) {
-            imageProcessor.stop();
-        }
-
-        try {
-            switch (selectedModel) {
-
-                case POSE_DETECTION:
-                    PoseDetectorOptionsBase poseDetectorOptions =
-                            PreferenceUtils.getPoseDetectorOptionsForLivePreview(this);
-                    boolean shouldShowInFrameLikelihood =
-                            PreferenceUtils.shouldShowPoseDetectionInFrameLikelihoodLivePreview(this);
-                    boolean visualizeZ = PreferenceUtils.shouldPoseDetectionVisualizeZ(this);
-                    boolean rescaleZ = PreferenceUtils.shouldPoseDetectionRescaleZForVisualization(this);
-                    boolean runClassification = PreferenceUtils.shouldPoseDetectionRunClassification(this);
-                    imageProcessor =
-                            new PoseDetectorProcessor(
-                                    this,
-                                    poseDetectorOptions,
-                                    shouldShowInFrameLikelihood,
-                                    visualizeZ,
-                                    rescaleZ,
-                                    runClassification,
-                                    /* isStreamMode = */ true);
-                    break;
-
-                default:
-                    throw new IllegalStateException("Invalid model name");
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Can not create image processor: " + selectedModel, e);
-            Toast.makeText(
-                    getApplicationContext(),
-                    "Can not create image processor: " + e.getLocalizedMessage(),
-                    Toast.LENGTH_LONG)
-                    .show();
-            return;
-        }
-
-        ImageAnalysis.Builder builder = new ImageAnalysis.Builder();
-        Size targetResolution = PreferenceUtils.getCameraXTargetResolution(this, lensFacing);
-        if (targetResolution != null) {
-            builder.setTargetResolution(targetResolution);
-        }
-        analysisUseCase = builder.build();
-
-        needUpdateGraphicOverlayImageSourceInfo = true;
-        analysisUseCase.setAnalyzer(
-                // imageProcessor.processImageProxy will use another thread to run the detection underneath,
-                // thus we can just runs the analyzer itself on main thread.
-                ContextCompat.getMainExecutor(this),
-                imageProxy -> {
-                    if (needUpdateGraphicOverlayImageSourceInfo) {
-                        boolean isImageFlipped = lensFacing == CameraSelector.LENS_FACING_FRONT;
-                        int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
-                        if (rotationDegrees == 0 || rotationDegrees == 180) {
-                            graphicOverlay.setImageSourceInfo(
-                                    imageProxy.getWidth(), imageProxy.getHeight(), isImageFlipped);
-                        } else {
-                            graphicOverlay.setImageSourceInfo(
-                                    imageProxy.getHeight(), imageProxy.getWidth(), isImageFlipped);
-                        }
-                        needUpdateGraphicOverlayImageSourceInfo = false;
-                    }
-                    try {
-                        imageProcessor.processImageProxy(imageProxy, graphicOverlay);
-                    } catch (MlKitException e) {
-                        Log.e(TAG, "Failed to process image. Error: " + e.getLocalizedMessage());
-                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
-
-        cameraProvider.bindToLifecycle(/* lifecycleOwner= */ this, cameraSelector, analysisUseCase);
     }
 
     private void InitalUI() {//一開始為休息狀態
